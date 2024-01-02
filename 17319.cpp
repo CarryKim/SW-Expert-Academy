ea373ed362b628308d70fe1b77c6fcd8cdd2096b
CarryKim <137850094+CarryKim@users.noreply.github.com>
2024-01-02 17:48:12 +0900

    17319.cpp

---
+++ 17319.cpp
@@ -0,0 +1,25 @@
+// 17319. 문자열문자열
+// https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AYgEiwbKy48DFARP
+// Authorized by CarryKim
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    for(int i=1; i<=T; i++) {
+        int N;
+        cin >> N;
+        string S;
+        cin >> S;
+        if(S.size()%2) cout << "#" << i << " No\n";
+        else{
+            if(S == S.substr(S.size()/2) + S.substr(S.size()/2)) cout << "#" << i << " Yes\n";
+            else cout << "#" << i << " No\n";
+        }
+    }
+    return 0;
+}
